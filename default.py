6de4353321fac59503842e7161dfa1dc0ede1618
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-09 01:11:20 -0300

    default.py simplificado

--- default.py
+++ default.py
@@ -3,14 +3,10 @@
 
 """Main exectable module."""
 
-import gc
 import sys
 
 from resources.lib.log import log_msg
 from resources.lib.utils import entrypoint
-
-from resources.lib.progressbar import ProgressBar
-from resources.lib.database import Database
 
 from resources.test.tests import run_tests
 from resources.lib.menus.main import MainMenu
@@ -20,12 +16,11 @@ from resources.lib.menus.main import MainMenu
 def main():
     """Main entry point for addon."""
     if "test" in sys.argv:
+        log_msg("Starting tests...")
         run_tests()
-    else:
-        MainMenu(
-            database=Database(),
-            progressbar=ProgressBar()
-        ).view()
+        return
+
+    MainMenu().show()
 
 if __name__ == '__main__':
     main()
