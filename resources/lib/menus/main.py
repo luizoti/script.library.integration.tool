21f1249016758facb75aac97207fd2b6feec96b0
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-09 01:14:13 -0300

    rewrited with dialog_select.Select class

--- resources/lib/menus/main.py
+++ resources/lib/menus/main.py
@@ -5,15 +5,16 @@
 import sys
 
 import xbmc
-import xbmcgui
 
 from resources import ADDON_ID
 from resources import ADDON_NAME
 
-from resources.lib.misc import bold
-from resources.lib.misc import color
-from resources.lib.misc import getstring
-from resources.lib.misc import videolibrary
+
+from resources.lib.misc import color, getstring, videolibrary
+from resources.lib.dialog_select import Select
+
+from resources.lib.database import Database
+from resources.lib.progressbar import ProgressBar
 
 from resources.lib.menus.managed_movies import ManagedMoviesMenu
 from resources.lib.menus.staged_movies import StagedMoviesMenu
@@ -22,32 +23,21 @@ from resources.lib.menus.staged_tv import StagedTVMenu
 # from resources.lib.menus.synced import SyncedMenu
 from resources.lib.menus.blocked import BlockedMenu
 
-# from resources.lib.dialog_select import Select
-
 # TODO: automatically clean & update when adding/removing based in type and path
-# TODO: support a centralized managed_folder that's shared over network
 # TODO: rebuild library option
 #   1. FLAG all itens in managed
 #   2. move all all to staged
 #   3. delete all managed itens
 #   4. re-add all FLAGGED itens
-# TODO: integrate WatchedList
-# TODO: option to call WatchedList if it's installed after updating library
-# IDEA: use plugin menu system instead of dialog windows
-# TODO: Put all classes in their own file, change menu classes to ManagedMenu, StagedMenu, etc.
 
 
 class MainMenu():
-    """
-    Perform basic initialization of folder structure.
+    """Perform basic initialization of folder structure."""
 
-    Displays a window that leads to other menus.
-    """
-
-    def __init__(self, database, progressbar):
+    def __init__(self):
         """__init__ MainMenu."""
-        self.database = database
-        self.progressbar = progressbar
+        self.database = Database()
+        self.progressbar = ProgressBar()
         self.lastchoice = False
 
     def library_options(self):
@@ -56,49 +46,71 @@ class MainMenu():
             653: 'scan',
             14247: 'clean',
         }
-        selection = xbmcgui.Dialog().select(
-            heading=f"{ADDON_NAME} - {color(bold('Library options'))}",
-            list=[xbmc.getLocalizedString(x).title() for x in OPTIONS],
-            useDetails=True,
-            preselect=self.lastchoice
+        sel = Select(
+            heading=f"{ADDON_NAME} - {color('Library options')}",
+            turnbold=True
         )
-        self.lastchoice = selection
-        if selection >= 0:
-            arg = OPTIONS[list(OPTIONS.keys())[selection]]
-            if arg:
-                videolibrary(arg)
-                # sleep to wait library update, whithout this
-                xbmc.sleep(2500)
-            self.view()
-        else:
-            self.view()
-        sys.exit()
+        sel.items([
+            color(xbmc.getLocalizedString(x).title(), "darkgrey")
+                for x in OPTIONS
+            ],
+            turnbold=False
+        )
+        selection = sel.show(
+            useDetails=True,
+            preselect=self.lastchoice,
+            back=True
+        )
+        try:
+            self.lastchoice = selection["index1"]
+        except TypeError:
+            pass
+        if selection:
+            if selection['type'] == 'item':
+                options_arg = OPTIONS[list(OPTIONS)[selection["index1"]]]
+                videolibrary(options_arg)
+                xbmc.sleep(1500)
+            self.library_options()
 
-    def view(self):
+    def show(self):
         """Display main menu which leads to other menus."""
+        self.lastchoice = False
         OPTIONS = {
-            32002: ManagedMoviesMenu(self.database, self.progressbar).view_all,
-            32004: StagedMoviesMenu(self.database, self.progressbar).view_all,
-            32003: ManagedTVMenu(self.database, self.progressbar).view_shows,
-            32005: StagedTVMenu(self.database, self.progressbar).view_shows,
+            32002: ManagedMoviesMenu(self.database, self.progressbar).show_all,
+            32004: StagedMoviesMenu(self.database, self.progressbar).show_all,
+            32003: ManagedTVMenu(self.database, self.progressbar).show_all,
+            32005: StagedTVMenu(self.database, self.progressbar).show_all,
             # 32006: SyncedMenu(self.database, self.progressbar).view,
-            32007: BlockedMenu(self.database, self.progressbar).view,
+            32007: BlockedMenu(self.database, self.progressbar).show_all,
+        }
+        EXTRA_OPTIONS = {
             32180: self.library_options,
             32179: xbmc.executebuiltin,
         }
-        selection = xbmcgui.Dialog().select(
-            heading=bold(ADDON_NAME),
-            list=[getstring(x) for x in OPTIONS],
-            useDetails=True,
-            preselect=self.lastchoice
+        sel = Select(
+            heading=ADDON_NAME,
+            turnbold=True
         )
-        self.lastchoice = selection
-        if selection >= 0:
-            command = OPTIONS[list(OPTIONS.keys())[selection]]
-            if command:
-                if xbmc.executebuiltin == command:
-                    command(f"Addon.OpenSettings({ADDON_ID})")
-                    return
+        sel.items([getstring(x) for x in OPTIONS])
+        sel.extraopts([getstring(x) for x in EXTRA_OPTIONS])
+        selection = sel.show(
+            useDetails=True,
+            preselect=self.lastchoice,
+            back=False
+        )
+        if selection:
+            if selection['type'] == 'item':
+                command = OPTIONS[list(OPTIONS)[selection['index1']]]
                 command()
-            self.view()
+            elif selection['type'] == 'opt':
+                selected = list(EXTRA_OPTIONS)[selection['index1']]
+                command = EXTRA_OPTIONS[list(EXTRA_OPTIONS)[selection['index1']]]
+                if int(selected) == 32179:
+                    # Open addon settings, the second argument turn a blocking task
+                    command(f"Addon.OpenSettings({ADDON_ID})", True)
+                else:
+                    # Open library options
+                    command()
+                # When closed, open the MainMenu again
+            self.show()
         sys.exit()
