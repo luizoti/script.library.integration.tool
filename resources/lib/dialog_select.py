81442e24490b5e64c1873eea403b6099dac9d02b
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-10 13:23:12 -0300

    Simplified Custom Select, simpler api

--- resources/lib/dialog_select.py
+++ resources/lib/dialog_select.py
@@ -2,83 +2,82 @@
 
 """Custon xbmcgui.Dialog.select."""
 
+import xbmc
 import xbmcgui
-
-from resources import ADDON
-from resources import ADDON_NAME
-
-
-def _bold(string):
-    """
-    Return string formated with a bold.
-    """
-    return f"[B]{string}[/B]"
-
-
-def _getstring(string_id):
-    """Shortcut function to return string from String ID."""
-    return str(ADDON.getLocalizedString(string_id))
+from resources import ADDON, ADDON_NAME
 
 
 class Select(xbmcgui.Dialog):
-    """Module to provide a Constom xbmcgui.Dialog."""
+    """Module to provide a Constom xbmcgui.Dialog.
 
-    def __init__(self, heading=ADDON_NAME, turnbold=False):
-        """CustonDialog __init__."""
+    return:
+        Key ESC/Backspace will return -> None
+        Key Back will return          -> 'back'
+        All other options, returns their representation in the dictionary are all.
+    """
+
+    def __init__(
+        self,
+        heading=ADDON_NAME,
+        turnbold=False,
+        back_option=32011,
+    ) -> None:
+        """CustonDialogSelect __init__."""
         super(__class__, self).__init__()
-        if turnbold:
-            self.heading = _bold(heading)
-        else:
-            self.heading = heading
-        self._list = None
-        self.listofitems = None
-        self.listofopts = None
-        self.extra = None
+        self.options_dict: dict = {}
+        self.options_strings: list = []
+        self.back_option = {back_option: "back"}
+        self.heading = self.bold(heading) if not turnbold else heading
 
-    def items(self, listofitems, turnbold=True):
+    @staticmethod
+    def bold(normal_string):
+        """Return a bold string."""
+        return f"[B]{normal_string}[/B]"
+
+    @staticmethod
+    def get_string(string_id):
+        """Shortcut function to return string from String ID."""
+        xbmc_string = xbmc.getLocalizedString(string_id).title()
+        if xbmc_string:
+            return xbmc_string
+        return ADDON.getLocalizedString(string_id)
+
+    def _parse_options(self, options: dict, turnbold=False):
+        """Convert a dict key to string."""
+        for key in options:
+            item_string = key if isinstance(key, str) else self.get_string(key)
+            if turnbold:
+                item_string = self.bold(item_string)
+            self.options_strings.append(item_string)
+
+    def options(self, options, turnbold=True):
         """Add item lines to dialog select."""
-        self.listofitems = [
-            _bold(bstr) for bstr in listofitems
-        ] if turnbold else listofitems
-        self._list = listofitems
+        self.options_dict.update(options)
+        self._parse_options(options=options, turnbold=turnbold)
 
-    def extraopts(self, listofopts):
+    def extra_options(self, options):
         """Add option lines to dialog select."""
-        self.listofopts = listofopts
-        self.extra = True
+        self.options_dict.update(options)
+        self._parse_options(options=options)
 
-    def show(self, autoclose=False, useDetails=False, preselect=False, back=False, back_value=_getstring(32011)):
+    def show(self, autoclose=False, useDetails=False, preselect=False) -> tuple:
         """Open dialog select with all params."""
-        _type = None
-        selection1 = None
-        if self.extra:
-            self.listofitems += self.listofopts
-        if back:
-            self.listofitems += [back_value]
-        selection = self.select(
+        # False returns "Programs" string
+        # check if self.back_option key is not False
+        if False not in self.back_option:
+            self.options_dict.update(self.back_option)
+            self._parse_options(self.back_option)
+        selected = self.select(
             heading=self.heading,
-            list=self.listofitems,
+            list=self.options_strings,
             autoclose=autoclose,
             preselect=preselect,
-            useDetails=useDetails
+            useDetails=useDetails,
         )
-        try:
-            str_opt = self._list[selection]
-        except IndexError:
-            str_opt = self.listofitems[selection]
-        if selection == -1:
+        # If ESC/Backspace or option Back...
+        if selected == -1:
             return None
-        elif str_opt == back_value:
-            return
-        if self.listofopts:
-            if str_opt in self.listofopts:
-                _type = 'opt'
-                for i, _str in enumerate(self.listofopts):
-                    if _str in str_opt:
-                        selection1 = i
-        if str_opt in self._list or '[B]' in str_opt:
-            _type = 'item'
-            for i, _str in enumerate(self._list):
-                if _str == str_opt:
-                    selection1 = i
-        return {'index': selection, 'index1': selection1, 'str': str_opt, 'type': _type}
+        for index, option in enumerate(self.options_dict.items()):
+            if selected == index:
+                return index, *option
+        return None
--- resources/lib/menus/main.py
+++ resources/lib/menus/main.py
@@ -5,23 +5,18 @@
 import sys
 
 import xbmc
-
-from resources import ADDON_ID
-from resources import ADDON_NAME
-
-
-from resources.lib.misc import color, get_string, videolibrary
+from resources import ADDON_ID, ADDON_NAME
+from resources.lib.database import Database
 from resources.lib.dialog_select import Select
 
-from resources.lib.database import Database
-from resources.lib.progressbar import ProgressBar
-
-from resources.lib.menus.managed_movies import ManagedMoviesMenu
-from resources.lib.menus.staged_movies import StagedMoviesMenu
-from resources.lib.menus.managed_tv import ManagedTVMenu
-from resources.lib.menus.staged_tv import StagedTVMenu
 # from resources.lib.menus.synced import SyncedMenu
 from resources.lib.menus.blocked import BlockedMenu
+from resources.lib.menus.managed_movies import ManagedMoviesMenu
+from resources.lib.menus.managed_tv import ManagedTVMenu
+from resources.lib.menus.staged_movies import StagedMoviesMenu
+from resources.lib.menus.staged_tv import StagedTVMenu
+from resources.lib.misc import bold, color, videolibrary
+from resources.lib.progressbar import ProgressBar
 
 # TODO: automatically clean & update when adding/removing based in type and path
 # TODO: rebuild library option
@@ -31,86 +26,75 @@ from resources.lib.menus.blocked import BlockedMenu
 #   4. re-add all FLAGGED itens
 
 
-class MainMenu():
+class MainMenu:
     """Perform basic initialization of folder structure."""
 
     def __init__(self):
         """__init__ MainMenu."""
         self.database = Database()
         self.progressbar = ProgressBar()
-        self.lastchoice = False
+        self.lastchoice = -1
 
     def library_options(self):
         """Display dedicated menu to Library functions."""
-        OPTIONS = {
-            653: 'scan',
-            14247: 'clean',
-        }
-        sel = Select(
-            heading=f"{ADDON_NAME} - {color('Library options')}",
-            turnbold=True
+        select_menu = Select(
+            heading=bold(f"{ADDON_NAME} - {color('Library options')}"),
+            turnbold=True,
         )
-        sel.items([
-            color(xbmc.getLocalizedString(x).title(), "darkgrey")
-                for x in OPTIONS
-            ],
-            turnbold=False
+        select_menu.options(
+            {
+                653: "scan",
+                14247: "clean",
+            },
+            turnbold=False,
         )
-        selection = sel.show(
-            useDetails=True,
-            preselect=self.lastchoice,
-            back=True
-        )
-        try:
-            self.lastchoice = selection["index1"]
-        except TypeError:
-            pass
-        if selection:
-            if selection['type'] == 'item':
-                options_arg = OPTIONS[list(OPTIONS)[selection["index1"]]]
-                videolibrary(options_arg)
+        selected_option = select_menu.show(useDetails=True, preselect=self.lastchoice)
+
+        if selected_option:
+            selected_index, _, selected_value = selected_option
+            try:
+                self.lastchoice = selected_index
+            except TypeError:
+                pass
+            if selected_value == "back":
+                self.show()
+            else:
+                videolibrary(selected_value)
                 xbmc.sleep(1500)
-            self.library_options()
+                self.library_options()
+            return
 
     def show(self):
         """Display main menu which leads to other menus."""
         self.lastchoice = False
-        OPTIONS = {
-            32002: ManagedMoviesMenu(self.database, self.progressbar).show_all,
-            32004: StagedMoviesMenu(self.database, self.progressbar).show_all,
-            32003: ManagedTVMenu(self.database, self.progressbar).show_all,
-            32005: StagedTVMenu(self.database, self.progressbar).show_all,
-            # 32006: SyncedMenu(self.database, self.progressbar).view,
-            32007: BlockedMenu(self.database, self.progressbar).show_all,
-        }
-        EXTRA_OPTIONS = {
-            32180: self.library_options,
-            32179: xbmc.executebuiltin,
-        }
-        sel = Select(
-            heading=ADDON_NAME,
-            turnbold=True
+        select_menu = Select(heading=ADDON_NAME, turnbold=True, back_option=False)
+        select_menu.options(
+            {
+                32002: ManagedMoviesMenu(self.database, self.progressbar).show_all,
+                32004: StagedMoviesMenu(self.database, self.progressbar).show_all,
+                32003: ManagedTVMenu(self.database, self.progressbar).show_all,
+                32005: StagedTVMenu(self.database, self.progressbar).show_all,
+                # 32006: SyncedMenu(self.database, self.progressbar).view,
+                32007: BlockedMenu(self.database, self.progressbar).show_all,
+            }
         )
-        sel.options([get_string(x) for x in OPTIONS])
-        sel.extra_options([get_string(x) for x in EXTRA_OPTIONS])
-        selection = sel.show(
-            useDetails=True,
-            preselect=self.lastchoice,
-            back=False
+        select_menu.extra_options(
+            {
+                32180: self.library_options,
+                32179: xbmc.executebuiltin,
+            }
         )
-        if selection:
-            if selection['type'] == 'item':
-                command = OPTIONS[list(OPTIONS)[selection['index1']]]
-                command()
-            elif selection['type'] == 'opt':
-                selected = list(EXTRA_OPTIONS)[selection['index1']]
-                command = EXTRA_OPTIONS[list(EXTRA_OPTIONS)[selection['index1']]]
-                if int(selected) == 32179:
-                    # Open addon settings, the second argument turn a blocking task
-                    command(f"Addon.OpenSettings({ADDON_ID})", True)
-                else:
-                    # Open library options
-                    command()
-                # When closed, open the MainMenu again
-            self.show()
+        selected_option: tuple = select_menu.show(
+            useDetails=True, preselect=self.lastchoice
+        )
+        if selected_option:
+            _, selected_key, selected_value = selected_option
+            if selected_key == 32179:
+                # # Open addon settings, the second argument turn a blocking task
+                selected_value(f"Addon.OpenSettings({ADDON_ID})", True)
+                self.show()
+            if selected_option == "back":
+                self.show()
+            else:
+                selected_value()
         sys.exit()
