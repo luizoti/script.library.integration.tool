05351e2bd825a80f10d56602865c8ddea272ca9f
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-14 14:52:35 -0300

    fix back_option behavior

--- resources/lib/dialog_select.py
+++ resources/lib/dialog_select.py
@@ -24,9 +24,9 @@ class Select(xbmcgui.Dialog):
     ) -> None:
         """CustonDialogSelect __init__."""
         super(__class__, self).__init__()
+        self.back_option: int = back_option
         self.options_dict: dict = {}
         self.options_strings: list = []
-        self.back_option = {back_option: "back"}
         self.heading = self.bold(heading) if not turnbold else heading
 
     @staticmethod
@@ -44,11 +44,14 @@ class Select(xbmcgui.Dialog):
 
     def _parse_options(self, options: dict, turnbold=False):
         """Convert a dict key to string."""
+        item_string: str
         for key in options:
             item_string = key if isinstance(key, str) else self.get_string(key)
             if turnbold:
                 item_string = self.bold(item_string)
             self.options_strings.append(item_string)
+        # return last value of options list, for back_option
+        return {item_string: "back"}
 
     def options(self, options, turnbold=True):
         """Add item lines to dialog select."""
@@ -68,9 +71,9 @@ class Select(xbmcgui.Dialog):
         """
         # False returns "Programs" string
         # check if self.back_option key is not False
-        if False not in self.back_option:
+        if self.back_option:
+            self.back_option = self._parse_options({self.back_option: "back"})
             self.options_dict.update(self.back_option)
-            self._parse_options(self.back_option)
         selected = self.select(
             heading=self.heading,
             list=self.options_strings,
