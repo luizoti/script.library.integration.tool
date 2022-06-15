4cc92750ea6c37fbdda4adefff8256d21b4b7c8e
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-15 14:48:50 -0300

    changed log message format

--- resources/lib/logger.py
+++ resources/lib/logger.py
@@ -21,7 +21,7 @@ class KodiLogHandler(logging.StreamHandler):
         logging.StreamHandler.__init__(self)
 
         formatter = logging.Formatter(
-            "[{}][v{}][%(name)s] --> %(message)s".format(ADDON_NAME, ADDON_VERSION)
+            "[{}][%(name)s] -> %(message)s".format(ADDON_NAME)
         )
         self.setFormatter(formatter)
 
