7f20f7418e52c9723ef1ec779cd4d94d0ff5f1ab
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-21 10:59:39 -0300

    refactor: :art: added MovieFileCreator based in Movie class
    This dataclass merge older CreateNfo class and is desined only form movies, this will create nfo, strm and in future all other types of files

--- resources/lib/content/movie/movie.py
+++ resources/lib/content/movie/movie.py
@@ -36,3 +36,55 @@ class Movie(Content):
         return f"{self.managed_movie_dir}/{self.formed_title}.strm"
 
 
+@dataclass
+class MovieFileCreator(Movie):
+    """Create files related with content item Movie."""
+
+    def nfo_body_string(self) -> str:
+        """Return str title formated with file path."""
+        nfo_info_body = "".join(
+            [
+                f"\t<title>{self.title}</title>\n",
+                f"\t<year>{self.year}</year>\n",
+                f"\t<original_filename>{self.file}</original_filename>\n",
+            ]
+        )
+        return f'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<movie>\n{nfo_info_body}</movie>'
+
+    @property
+    def asdict(self) -> dict:
+        """Return a dict from dataclass.
+        Returns:
+            dict: dict with dataclass properties.
+        """
+        return asdict(self)
+
+    def create_nfo(self) -> bool:
+        """Create stream file with self.file at self.movie_strm filepath."""
+        mkdir(self.managed_movie_dir)
+        with xbmcvfs.File(self.movie_nfo, "w+") as nfofile:
+            try:
+                nfofile.write(self.nfo_body_string())
+                LOG.info("Created NFO file %s", self.movie_nfo)
+                # TODO: What was the reason for updating the title? Do not remember.
+                # self.update_item_title(file=self.file, _type="movie", title=self.title)
+                return True
+            except Exception:
+                LOG.exception("CreateNfo.create:")
+            finally:
+                nfofile.close()
+        return None
+
+    def create_strm(self):
+        """Create stream file with self.file at self.movie_strm filepath."""
+        mkdir(self.managed_movie_dir)
+        with xbmcvfs.File(self.movie_strm, "w+") as strm:
+            try:
+                strm.write(self.file)
+                LOG.debug("Created STRM file %s", self.movie_strm)
+                return True
+            except Exception:
+                LOG.exception("filesystem.create_stream_file:")
+            finally:
+                strm.close()
+        return None
