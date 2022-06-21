74aefe0d903104ed424f84318c2546d1ad58a108
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-21 10:54:05 -0300

    added Content dataclass base of other classes

---
+++ resources/lib/content/content.py
@@ -0,0 +1,43 @@
+"""A base dataclass module with common properties and methods for movies and shows."""
+
+from dataclasses import asdict, dataclass
+
+
+@dataclass
+class Content:
+    """Class to build information aboult movies."""
+
+    file: str
+    title: str
+    type: str
+    status: str
+    year: int
+
+    def __str__(self):
+        """Return str title formated with file path."""
+        return f"[B]{self.title}[/B] - [I]{self.file}[/I]"
+
+    @property
+    def formed_title(self) -> str:
+        """Return title formated with formed_year.
+
+        Returns:
+            str: title (year)
+        """
+        return f"{self.title} {self.formed_year}"
+
+    @property
+    def formed_year(self) -> str:
+        """Return year inside parentheses.
+        Returns:
+            str: (year)
+        """
+        return f"({self.year})"
+
+    @property
+    def asdict(self) -> dict:
+        """Return a dict from dataclass.
+        Returns:
+            dict: dict with dataclass properties.
+        """
+        return asdict(self)
