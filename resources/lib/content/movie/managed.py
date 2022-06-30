12405db7a89192acb7842350993c214fcbdea920
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-06-30 10:24:07 -0300

    added a managed movie module in content

---
+++ resources/lib/content/movie/managed.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from dataclasses import InitVar, dataclass
+
+from resources.lib.content.movie.movie import MovieFileManager
+from resources.lib.database.database import DBCommon
+
+
+@dataclass
+class ManagedMovie(MovieFileManager):
+    """Docstring for ManagedMovie.
+
+    Managed create and delete files.
+    """
+
+    database: InitVar[DBCommon]
+
+    def __post_init__(self, database: DBCommon):
+        self.database = database
+
+    def move_to_staged(self):
+        """Remove item from managed (library), move to staged and delete all files if exist."""
+        self.delete_managed_movie_diretory()
+        self.database.update_status(file=self.file, _type="movie", status="staged")
+
+    def remove_completely(self):
+        """Complete remove item from database and all files if exist."""
+        self.delete_managed_movie_diretory()
+        self.database.delete(file=self.file, _type="movie")
+
+    def remove_completely_and_block(self):
+        """Similar to remove_completely, but block item."""
+        self.remove_completely()
+        self.database.block(value=self.title, _type="movie")
