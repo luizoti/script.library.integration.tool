fd3da270f3239b055232e9cd8d3784da6fca14ca
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-10-01 00:53:36 -0300

    clearer names for classes and variables

--- resources/lib/gui/progressbar.py
+++ resources/lib/gui/progressbar.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Custon xbmcgui.DialogProgress."""
+"""Custom xbmcgui.DialogProgress."""
 
 import logging
 import sys
@@ -13,7 +13,7 @@ LOG = logging.getLogger(__name__)
 
 
 class ProgressBar(xbmcgui.DialogProgress):
-    """Module to provide a Constom xbmcgui.DialogProgress."""
+    """Module to provide a Custom xbmcgui.DialogProgress."""
 
     def __init__(self) -> None:
         """ProgressBar __init__."""
@@ -23,53 +23,52 @@ class ProgressBar(xbmcgui.DialogProgress):
     def __del__(self) -> None:
         LOG.debug("""ProgressBar closed.""")
 
-    def create_progressdialog(self, head=None, msg=""):
+    def create_progress_dialog(self, head=None, msg=""):
         """Method to create ProgressBar window"""
         if not head:
             LOG.critical("Select() head argument cannot be None")
             return
         self.create(head, msg)
 
-    def update_progressdialog(self, perc, msg):
+    def update_progress_dialog(self, percentage, message):
         """Method to update ProgressBar window."""
-        if self.iscanceled():
-            self.close_progressdialog()
-        self.update(int(100 * perc), msg)
+        self.update(int(100 * percentage), message)
         xbmc.sleep(200)
 
-    def close_progressdialog(self, exit=None):
+    def close_progress_dialog(self):
         """Close method to close progress by normal progress finish."""
-        self.close()
-        if exit:
-            sys.exit()
+        if self.iscanceled():
+            self.close()
+            return True
+        return False
 
 
-class BGProgressBar(xbmcgui.DialogProgressBG):
-    """Module to provide a Constom xbmcgui.DialogProgressBG."""
+class ProgressBarBackground(xbmcgui.DialogProgressBG):
+    """Module to provide a Custom xbmcgui.DialogProgressBG."""
 
     def __init__(self):
-        """BGProgressBar __init__."""
+        """ProgressBarBackground __init__."""
         super(__class__, self).__init__()
-        LOG.debug("""BGProgressBar __init__.""")
+        LOG.debug("""ProgressBarBackground __init__.""")
 
-    def create_progressbar(self, head=None, msg=""):
-        """Method to create BGProgressBar window"""
+    def create_progress_bar(self, head=None, message=""):
+        """Method to create ProgressBarBackground window"""
         if not head:
             LOG.critical("Select() head argument cannot be None")
             return
-        self.create(head, msg)
+        self.create(head, message)
 
-    def update_progress_bar(self, perc, msg):
-        """Method to update BGProgressBar window."""
+    def update_progress_bar(self, percentage, message):
+        """Method to update ProgressBarBackground window."""
         if self.isFinished():
             xbmc.sleep(100)
-            self.close_progressbar()
-        self.update(int(perc), msg)
+            self.close_progress_bar()
+        self.update(int(percentage), message)
 
-    def close_progressbar(self, exit=None):
+    def close_progress_bar(self, _exit=None):
         """Close method to close progress by normal progress finish."""
         self.close()
-        # notification("Background Desfazendo ultumas ações!", 3000)
+        # notification("Background Desfazendo ultimas ações!", 3000)
         # Exec operations
-        if exit:
+        if _exit:
             sys.exit()
